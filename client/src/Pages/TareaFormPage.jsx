663eacf8ee67a036c3bc0585153306f356f1fb01
Ibarahona73 <igyt2015@gmail.com>
2025-05-03 19:01:37 -0500

    fds

--- client/src/Pages/TareaFormPage.jsx
+++ client/src/Pages/TareaFormPage.jsx
@@ -1,8 +1,38 @@
-import React from "react";
+import {useForm} from "react-hook-form";
+
 
 export function TareaFormPage(){
+
+    const {register,handleSubmit,formState:{errors}}=useForm();
+    const {onSubmit}=handleSubmit((data)=>{
+        console.log(data);
+        fetch("https://backend-ny2d.onrender.com/api/v1/tareas/",{
+
+            /* method:"POST",
+            headers:{
+                "Content-Type":"application/json",
+                "Accept":"application/json"
+            },
+            body:JSON.stringify(data)
+        })
+        .then((response)=>response.json())
+        .then((data)=>console.log(data))
+        .catch((error)=>console.log(error)) */
+    })
+
     return(
-        <div>TareaFormPage</div>
-    )
+        <div>
+            <form action="https://backend-ny2d.onrender.com/api/v1/tareas/" method="POST" className="formulario">
+                <h1>Formulario de Tarea</h1>
+                <input type="text" placeholder="Nombre de la tarea"                
+                {...register("titulo",{required:true})}
+                />
+                
+                <textarea rows="3" placeholder="Descripción de la tarea" 
+                {...register("descripcion",{required:true})}/>
+
+            </form>
+        </div>
+    )})
 }
 
